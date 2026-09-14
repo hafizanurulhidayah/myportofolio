920e1161f4de4bd75af65c09a2f18356be62c034
hafizanurulhidayah <hafiza.nurul@ui.ac.id>
2026-09-14 21:34:39 +0700

    fix: adjust css for experience n education using model

--- main/views.py
+++ main/views.py
@@ -5,16 +5,22 @@ from main.models import Education
 
 
 def show_main(request):
+    experience_list = Experience.objects.all()
+    education_list = Education.objects.all()
+
     context = {
         "name": "Hafiza Nurul Hidayah",
         "npm": "2506624101",
         "study_program": "S1 Ilmu Komputer",
         "bio": (
-           "Im currently pursuing a Computer Science degree at Universitas Indonesia "
-           "Actively exploring software engineering and collaborative tech initiatives. "
-           "Im always open to connecting and sharing insights on technology."
+            "Im currently pursuing a Computer Science degree at Universitas Indonesia "
+            "Actively exploring software engineering and collaborative tech initiatives. "
+            "Im always open to connecting and sharing insights on technology."
         ),
+        "experience_list": experience_list,
+        "education_list": education_list,
     }
+
     return render(request, "index.html", context)
 
 
@@ -24,6 +30,7 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
 
 def show_education(request):
     education_list = Education.objects.all()
