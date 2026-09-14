ee34cef5f5dea28390e557267b63b33df7bbf053
hafizanurulhidayah <hafiza.nurul@ui.ac.id>
2026-09-14 17:18:13 +0700

    fix: configure trusted origin for PWS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -157,3 +157,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://hafiza-nurul-myportofolio.pws.cs.ui.ac.id",
+]
